88c857886430ea65577f5d02da7c6161c5b3851f
VisPatrick <pksantos1003@gmail.com>
2025-07-01 20:57:41 -0300

    Prijeto

---
+++ aula21/atividade.py
@@ -0,0 +1,9 @@
+# Atividade
+# O Secretário de estado da Polícia Militar, ligou pra você e pediu que o auxiliasse em um estudo sobre os Roubos de Cargas registrados nos Batalhões de Polícia Militar, entre os anos de 2022 e 2023.
+# Ele te pediu o seguinte:
+# Verificar se existem Batalhões de Polícia Militar que estão registrando muitos Roubos de Cargas, além do que pode ser considerado normal, dado os registros dos outros BPMs
+# Verificar se há um padrão de registros de ocorrências, de modo que seja possível definir uma média de Roubos de Cargas através do BPMs, para ser informada a mídia
+# Gostaria que me enviasse isso de forma gráfica:
+# Se houver BPMs com registros de Roubos de Cargas muito acima dos demais, exiba o ranking desses BPMs. Caso não haja, exiba no ranking de todos os BPMs, através dos registros de Roubos de Cargas
+# Exiba também métricas que possam sustentar o argumento de que a média é uma medida confiável para ser utilizada e que o conjunto de dados possui um determinado padrão de registros dessas ocorrências
+
