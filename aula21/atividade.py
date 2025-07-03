51cba50180d99d7a4d11746bad41a4044ce33872
VisPatrick <pksantos1003@gmail.com>
2025-07-03 18:46:36 -0300

    plotando os dados

--- aula21/atividade.py
+++ aula21/atividade.py
@@ -146,3 +146,87 @@ try:
 except Exception as e:
     print(f'Erro ao obter informações sobre padrão de roubo de cargas: {e}')
     exit()
+
+
+import matplotlib.pyplot as plt
+
+
+# Plotando os dados
+try:
+    # import matplotlib.pyplot as plt    
+    plt.subplots(2, 2, figsize=(18, 12))
+    plt.suptitle('Análise de roubo de cargas RJ') 
+
+    # POSIÇÃO 01
+    # BOXPLOT
+    plt.subplot(2, 2, 1)  
+    plt.boxplot(array_roubo_carga, vert=False, showmeans=True)
+    plt.title("Boxplot dos Dados")
+
+    # POSIÇÃO 02
+    # MEDIDAS 
+    plt.subplot(2, 2, 2)
+    plt.title('Medidas Estatísticas')
+    plt.text(0.1, 0.9, f'Limite inferior: {limite_inferior}', fontsize=10)
+    plt.text(0.1, 0.8, f'Menor valor: {minimo}', fontsize=10) 
+    plt.text(0.1, 0.7, f'Q1: {q1}', fontsize=10)
+    plt.text(0.1, 0.6, f'Mediana: {mediana_roubo_carga}', fontsize=10)
+    plt.text(0.1, 0.5, f'Q3: {q3}', fontsize=10)
+    plt.text(0.1, 0.4, f'Média: {media_roubo_carga:.3f}', fontsize=10)
+    plt.text(0.1, 0.3, f'Maior valor: {maximo}', fontsize=10)
+    plt.text(0.1, 0.2, f'Limite superior: {limite_superior}', fontsize=10)
+
+    plt.text(0.5, 0.9, f'Distância Média e Mediana: {distancia:.4f}', fontsize=10)
+    plt.text(0.5, 0.8, f'IQR: {iqr}', fontsize=10)
+    plt.text(0.5, 0.7, f'Amplitude Total: {amplitude_total}', fontsize=10)
+    plt.text(0.5, 0.6, f'Variância: {variancia:.4f}', fontsize=10)
+    plt.text(0.5, 0.5, f'Distancia da média e Variância: {distancia_variancia_media}', fontsize=10)
+    plt.text(0.5, 0.4, f'Coeficiente de Variação: {coeficiente_variacao}', fontsize=10)
+    plt.text(0.5, 0.3, f'Desvio Padrão: {desvio_padrao:.4f}', fontsize=10)
+
+    plt.xticks([])
+    plt.yticks([])
+
+
+    # POSIÇÃO 03
+    # HISTOGRAMA DA DISTRIBUIÇÃO
+    plt.subplot(2, 2, 3)
+    plt.title('Concentração das Distribuições')
+    # Histograma
+    plt.hist(array_roubo_carga, bins=77, edgecolor='violet')
+
+    
+    # POSIÇÃO 04
+    # OUTLIERS SUPERIORES
+    plt.subplot(2, 2, 4)
+    plt.title('Outliers Superiores')
+    if not df_roubo_carga_outliers_superiores.empty:
+        dados_superiores = df_roubo_carga_outliers_superiores.sort_values(by='roubo_carga', ascending=True)
+
+        # Cria o gráfico e guarda as barras
+        barras = plt.barh(dados_superiores['munic'], dados_superiores['roubo_carga'], color='green')
+        # Adiciona rótulos nas barras
+        plt.bar_label(barras, fmt='%.0f', label_type='edge', fontsize=8, padding=2)
+
+
+        # Diminui o tamanho da fonte dos eixos
+        plt.xticks(fontsize=5)
+        plt.yticks(fontsize=5)
+
+        plt.title('Outliers Superiores')
+        plt.xlabel('Total Roubos de cargas')    
+    else:
+        # Se não houver outliers superiores, exibe uma mensagem no lugar.
+        plt.text(0.5, 0.5, 'Sem outliers superiores', ha='center', va='center', fontsize=12)
+        plt.title('Outliers Superiores')
+        plt.xticks([])
+        plt.yticks([])
+
+    # Ajusta os espaços do layout para que os gráficos não fiquem espremidos
+    plt.tight_layout()
+    # Mostra o painel
+    plt.show()
+
+except Exception as e:
+    print(f'Erro ao plotar {e}')
+    exit()
