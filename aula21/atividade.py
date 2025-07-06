50be4e8c6c65d2613ea54641c984fe58678f04d3
VisPatrick <pksantos1003@gmail.com>
2025-07-06 17:10:43 -0300

    Atualizando codigo

--- aula21/atividade.py
+++ aula21/atividade.py
@@ -9,12 +9,12 @@
 
 from sqlalchemy import create_engine
 import pandas as pd
-import numpy as np 
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 host = 'localhost'
 user = 'root'
-password = ''
+password = '0933'
 database = 'bd_roubos_cargas'
 
 
@@ -38,124 +38,99 @@ try:
         .reset_index()
     )
 
-
 except Exception as e:
     print(f'Erro: {e}')
 
 try:
-    print("Obtendo informações do roubo de carga.")
-
+    print("\nObtendo informações do roubo de carga.")
+    print(30 * '-')
     array_roubo_carga = np.array(df_ocorrencia_roubo['roubo_carga'])
-
-
     media_roubo_carga = np.mean(array_roubo_carga)
     mediana_roubo_carga = np.median(array_roubo_carga)
     distancia = abs((media_roubo_carga) / mediana_roubo_carga)
-    print(f'distancia: {distancia}')
-
-
-
-    q1 = np.quantile(array_roubo_carga, 0.25)
-    q2 = np.quantile(array_roubo_carga, 0.50)
-    q3 = np.quantile(array_roubo_carga, 0.75)
-
-    iqr = q3 - q1
-    limite_inferior = q1 - (iqr * 1.5)
-    limite_superior = q3 + (iqr * 1.5)
-
-    print(f'iqr: {iqr}')
-    print(f'limite inferior: {limite_inferior}')
-    print(f'limite superior: {limite_superior}')
-
+    #   QUARTIS
+    q1 = np.quantile(array_roubo_carga, 0.25, method='weibull')
+    q2 = np.quantile(array_roubo_carga, 0.50, method='weibull')
+    q3 = np.quantile(array_roubo_carga, 0.75, method='weibull')
+    #   AMPLITUDE
     maximo = np.max(array_roubo_carga)
     minimo = np.min(array_roubo_carga)
-    amplitude_total = maximo - minimo
-
+    amplitude = maximo - minimo
+    # MUNICÍPIOS COM MAIOR E MENOR NÚMERO DE ROUBOS DE CARGA
+    df_ocorrencia_roubo_menores = df_ocorrencia_roubo[
+        df_ocorrencia_roubo['roubo_carga'] < q2]
+    df_ocorrencia_roubo_maiores = df_ocorrencia_roubo[
+        df_ocorrencia_roubo['roubo_carga'] > q3]
+    #   IQR
+    iqr = q3 - q1
+    #   limite superior e inferior
+    limite_superior = q3 + 1.5 * iqr
+    limite_inferior = q1 - 1.5 * iqr
+    #   MEDIDAS DE DISPERSÃO
+    variancia = np.var(array_roubo_carga)
+    desvio_padrao = np.std(array_roubo_carga)
+    distancia_var_media = (variancia / media_roubo_carga ** 2)
+    coef_variacao = (desvio_padrao / media_roubo_carga) * 100
+    #   ASSIMETRIA
     assimetria = df_ocorrencia_roubo['roubo_carga'].skew()
+    #   CURTOSE
     curtose = df_ocorrencia_roubo['roubo_carga'].kurtosis()
 
-    df_base_roubo_carga_menores = df_ocorrencia_roubo[
-        df_ocorrencia_roubo['roubo_carga'] < q2]
-    df_base_roubo_carga_maiores = df_ocorrencia_roubo[
-        df_ocorrencia_roubo['roubo_carga'] > q3]
-
-    variancia = np.var(array_roubo_carga)
-    distancia_variancia_media = variancia / (media_roubo_carga **2)
-    desvio_padrao = np.std(array_roubo_carga)
-    coeficiente_variacao = desvio_padrao / media_roubo_carga
-
+    print("\nMenores registros de Roubos de Carga: ")
+    print(30 * '-')
+    print(df_ocorrencia_roubo_menores.sort_values(by='roubo_carga', ascending=True))
+    print("\nMaiores registros de Roubos de Carga: ")
+    print(30 * '-')
+    print(df_ocorrencia_roubo_maiores.sort_values(by='roubo_carga', ascending=False))
+    #   printando as medidas
+    print(f'\nMEDIDAS')
+    print(30 * '-')
+    print(f'limite inferior: {limite_inferior}')
+    print(f'Minimo: {minimo}')
+    print(f'Quartil 1: {q1}')
+    print(f'Quartil 2: {q2}')
+    print(f'Quartil 3: {q3}')
+    print(f'IQR: {iqr}')
+    print(f'Máximo: {maximo}')
+    print(f'limite superior: {limite_superior}')
+    print(f'Amplitude: {amplitude}')
+    print(f'Média: {media_roubo_carga:.2f}')
+    print(f'Mediana: {mediana_roubo_carga}')
+    print(f'Distância entre Média e Mediana: {distancia:.2f}')
+    print(f'Variância: {variancia:.2f}')
+    print(f'Desvio Padrão: {desvio_padrao:.2f}')
+    print(f'Distância Variância/Média: {distancia_var_media:.2f}')
+    print(f'Coeficiente de Variação: {coef_variacao:.2f}%')
+    print(f'Assimetria: {assimetria:.2f}')
+    print(f'Curtose: {curtose:.2f}')
+    #   OUTLIERS
     df_roubo_carga_outliers_inferiores = df_ocorrencia_roubo[
         df_ocorrencia_roubo['roubo_carga'] < limite_inferior]
     df_roubo_carga_outliers_superiores = df_ocorrencia_roubo[
         df_ocorrencia_roubo['roubo_carga'] < limite_superior]
 
-    print(f'media {media_roubo_carga:.2f}')
-    print(f'mediana: {mediana_roubo_carga:.2f}')
-    print(f'Distância da media e mediana: {distancia}')
-
-    print('Q1:', q1)
-    print('Q2:', q2)
-    print('Q3:', q3)
-
-    print('Maximo:', maximo)
-    print('Minimo:', minimo)
-    print('Amplitude:', amplitude_total)
-
-    print('Maiores:', df_base_roubo_carga_maiores)
-    print('Menores:', df_base_roubo_carga_menores)
-
-    print("\nQUARTIS")
-    print(30 * '-')
-    print(f'Q1: {q1}')
-    print(f'Q2: {q2}')
-    print(f'Q3: {q3}')
-    print(f'IQR: {iqr}')
-    print(f'Limite inferior: {limite_inferior}')
-    print(f'Limite superior: {limite_superior}')
-
-    print(f'Máximo: {maximo}')
-    print(f'Mínimo: {minimo}')
-    print(f'Amplitude total: {amplitude_total}')
-    print(f'Variancia: {variancia}')
-    print(f'Distância entre variância e média: {distancia_variancia_media}')
-    print(f'Desvio padrão: {desvio_padrao}')
-    print(f'Coeficiente de variação: {coeficiente_variacao}')
-    print(f'Assimetria : {assimetria}')
-    print(f'Curtose: {curtose}')
-
-    print("\nMENORES ROUBOS")
-    print(30 * '-')
-    print(df_base_roubo_carga_menores.sort_values(by='roubo_carga', ascending=True))
-    print("\nMAIORES ROUBOS")
-    print(df_base_roubo_carga_maiores.sort_values(by='roubo_carga', ascending=False))
-
-    print("\nOUTLIERS INFERIORES")
-    print(30 * '-')
-    if len(df_roubo_carga_outliers_inferiores) > 0:
-        print(df_roubo_carga_outliers_inferiores.sort_values(by='roubo_carga', ascending=False))
+    print('\nMunicípios com outliers inferiores: ')
+    print(45*'-')
+    if len(df_roubo_carga_outliers_inferiores) == 0:
+        print('Não existem outliers inferiores!')
     else:
-        print("Não há outliers inferiores")
+        print(df_roubo_carga_outliers_inferiores.sort_values(by='roubo_carga', ascending=True))
 
-        # OUTLIERS SUPERIORES
-    print("\nOUTLIERS SUPERIORES")
-    print(30 * '-')
-    if len(df_roubo_carga_outliers_superiores) > 0:
+    print('\nMunicípios com outliers superiores: ')
+    print(45*'-')
+    if len(df_roubo_carga_outliers_superiores) == 0:
+        print('Não existe outliers superiores!')
+    else:
         print(df_roubo_carga_outliers_superiores.sort_values(by='roubo_carga', ascending=False))
-    else:
-        print("Não há outliers superiores")
+
 except Exception as e:
-    print(f'Erro ao obter informações sobre padrão de roubo de cargas: {e}')
+    print(f'Erro ao obter informações do roubo de carga: {e}')
     exit()
-
-
-import matplotlib.pyplot as plt
-
-
 # Plotando os dados
 try:
-    # import matplotlib.pyplot as plt    
+    # import matplotlib.pyplot as plt
     plt.subplots(2, 2, figsize=(18, 12))
-    plt.suptitle('Análise de roubo de cargas RJ') 
+    plt.suptitle('Análise de roubo de cargas RJ')
 
     # POSIÇÃO 01
     # BOXPLOT
@@ -164,11 +139,11 @@ try:
     plt.title("Boxplot dos Dados")
 
     # POSIÇÃO 02
-    # MEDIDAS 
+    # MEDIDAS
     plt.subplot(2, 2, 2)
     plt.title('Medidas Estatísticas')
     plt.text(0.1, 0.9, f'Limite inferior: {limite_inferior}', fontsize=10)
-    plt.text(0.1, 0.8, f'Menor valor: {minimo}', fontsize=10) 
+    plt.text(0.1, 0.8, f'Menor valor: {minimo}', fontsize=10)
     plt.text(0.1, 0.7, f'Q1: {q1}', fontsize=10)
     plt.text(0.1, 0.6, f'Mediana: {mediana_roubo_carga}', fontsize=10)
     plt.text(0.1, 0.5, f'Q3: {q3}', fontsize=10)
@@ -176,17 +151,16 @@ try:
     plt.text(0.1, 0.3, f'Maior valor: {maximo}', fontsize=10)
     plt.text(0.1, 0.2, f'Limite superior: {limite_superior}', fontsize=10)
 
-    plt.text(0.5, 0.9, f'Distância Média e Mediana: {distancia:.4f}', fontsize=10)
+    plt.text(0.5, 0.9, f'Distância Média e Mediana: {distancia:.3f}', fontsize=10)
     plt.text(0.5, 0.8, f'IQR: {iqr}', fontsize=10)
-    plt.text(0.5, 0.7, f'Amplitude Total: {amplitude_total}', fontsize=10)
-    plt.text(0.5, 0.6, f'Variância: {variancia:.4f}', fontsize=10)
-    plt.text(0.5, 0.5, f'Distancia da média e Variância: {distancia_variancia_media}', fontsize=10)
-    plt.text(0.5, 0.4, f'Coeficiente de Variação: {coeficiente_variacao}', fontsize=10)
-    plt.text(0.5, 0.3, f'Desvio Padrão: {desvio_padrao:.4f}', fontsize=10)
+    plt.text(0.5, 0.7, f'Amplitude Total: {amplitude}', fontsize=10)
+    plt.text(0.5, 0.6, f'Variância: {variancia:.3f}', fontsize=10)
+    plt.text(0.5, 0.5, f'Distancia da média e Variância: {distancia_var_media}', fontsize=9)
+    plt.text(0.5, 0.4, f'Coeficiente de Variação: {coef_variacao}', fontsize=10)
+    plt.text(0.5, 0.3, f'Desvio Padrão: {desvio_padrao:.3f}', fontsize=10)
 
     plt.xticks([])
     plt.yticks([])
-
 
     # POSIÇÃO 03
     # HISTOGRAMA DA DISTRIBUIÇÃO
@@ -195,9 +169,9 @@ try:
     # Histograma
     plt.hist(array_roubo_carga, bins=77, edgecolor='violet')
 
-    
     # POSIÇÃO 04
-    # OUTLIERS SUPERIORES
+    # OUTLIERS SUPERIORES # diminuindo os valores para evitar que o gráfico fique muito grande
+
     plt.subplot(2, 2, 4)
     plt.title('Outliers Superiores')
     if not df_roubo_carga_outliers_superiores.empty:
@@ -206,8 +180,7 @@ try:
         # Cria o gráfico e guarda as barras
         barras = plt.barh(dados_superiores['munic'], dados_superiores['roubo_carga'], color='green')
         # Adiciona rótulos nas barras
-        plt.bar_label(barras, fmt='%.0f', label_type='edge', fontsize=8, padding=2)
-
+        plt.bar_label(barras, fmt='%.0f', label_type='edge', fontsize=6, padding=3)
 
         # Diminui o tamanho da fonte dos eixos
         plt.xticks(fontsize=5)
@@ -230,3 +203,4 @@ try:
 except Exception as e:
     print(f'Erro ao plotar {e}')
     exit()
+# These are recently edited files. Do not suggest code that has been deleted.
